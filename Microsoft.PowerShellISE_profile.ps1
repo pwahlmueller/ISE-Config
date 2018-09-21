f6daf26a356c30d4404f1c99886881fd33a5f45c
pwahlmueller <8706955+pwahlmueller@users.noreply.github.com>
2018-09-21 11:17:32 +0200

    Time and User in Title
    Added Time and user to the titlebar.
    Also added notice to install PsISEProjectExplorer and powershellhumanizer

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,8 +1,15 @@
+
 if (Get-Module -ListAvailable PsISEProjectExplorer)
 {Import-Module PsISEProjectExplorer}
-if (Get-Module -ListAvailable powershellhumanizer)
-{Import-Module powershellhumanizer}
+else {Write-Host "You could install-module PsISEProjectExplorer" -ForegroundColor Yellow}
 
+if (Get-Module -ListAvailable powershellhumanizer)
+
+{Import-Module powershellhumanizer}
+else {Write-Host "You could install-module powershellhumanizer" -ForegroundColor Yellow}
+
+ 
+#this won't display properly on Linux
 Function prompt {
     # .Description
     # This custom version of the PowerShell prompt will present a colorized location value based on the current provider. It will also display the PS prefix in red if the current user is running as administrator.    
@@ -34,8 +41,9 @@ Function prompt {
     }
     else {
         $adminfg = $host.ui.rawui.ForegroundColor
+        
     }
-
+ 
     Switch ((get-location).provider.name) {
         "FileSystem" { $fg = "green"}
         "Registry" { $fg = "magenta"}
@@ -58,6 +66,29 @@ Function prompt {
     Write-Host "PS " -nonewline -ForegroundColor $adminfg
     Write-Host "$($executionContext.SessionState.Path.CurrentLocation)" -foregroundcolor $fg -nonewline
     Write-Output "$('>' * ($nestedPromptLevel + 1)) "  
+
+    $code = 
+{
+    # thx for idea: http://community.idera.com/powershell/powertips/b/tips/posts/adding-live-clock-to-powershell-title-bar-part-2
+    param($RawUi, $ExecContext)
+
+    do
+    {
+        # find the current location in the host process
+        $location = $ExecContext.SessionState.Path.CurrentLocation
+        # compose the time and date display
+        $time = Get-Date -Format 'HH:mm:ss dddd MMMM d'
+        # compose the title bar text
+        $title = "$location     $time    " + (whoami)
+        # output the information to the title bar of the host process
+        $RawUI.WindowTitle = $title
+        # wait a half second
+        Start-Sleep -Milliseconds 500
+    } while ($true)
+}
+$ps = [PowerShell]::Create()
+$null = $ps.AddScript($code).AddArgument($host.UI.RawUI).AddArgument($ExecutionContext)
+$handle = $ps.BeginInvoke()
 }
 $host.PrivateData.ErrorForegroundColor = 'Yellow'
 write-host "Hallo $env:UserName on $env:ComputerName" -ForegroundColor Green -NoNewline
@@ -66,4 +97,5 @@ if ( (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.P
         write-host " as ADMIN" -foregroundcolor yellow -NoNewline
     }
 Get-Date
+
 
